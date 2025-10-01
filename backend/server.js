d12688eec19cc29dead12926146c521085bf244d
Oleg <sleite4r@gmail.com>
2025-10-01 21:25:23 +0200

    chore: translate server and MongoDB connection messages to English

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,38 @@
-const express = require("express"); 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-let products = [
-  { id: 1, navn: "Product A", pris: 100 },
-  { id: 2, navn: "Product B", pris: 200 },
-];
+mongoose
+  .conect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(() => console.error("Error connecting:", err));
 
-app.get("/products", (req, res) => {
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+app.get("/products", async (req, res) => {
+  const products = new Product.find();
   res.json(products);
 });
 
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req, body);
+  await newProduct.save();
+  res.json(newProduct);
+});
 
+const PORT = process.env.PORT || 3000;
 app.listen(3000, () => {
-  console.log("Serveren kører på http://localhost:3000");
+  console.log("The server is running on mongodb+srv");
 });
